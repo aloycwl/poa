c3d4703052b02245a4ab41d86f3e5f8c87ee19db
Aloysius <aloycwl@gmail.com>
2024-10-14 05:50:55 +0000

    init

---
+++ prysmctl.sh
@@ -0,0 +1,2 @@
+./prysmctl testnet generate-genesis --fork capella --num-validators 64 --genesis-time-delay 10 --chain-config-file config.yml --geth-genesis-json-in genesis.json  --geth-genesis-json-out genesis.json --output-ssz genesis.ssz
+./geth --datadir=gethdata init genesis.json
